05b3df82ced8adb0a9cd22183433162f89b5e1ce
dheenadhayalanperumal <69459677+dheenadhayalanperumal@users.noreply.github.com>
2024-09-23 12:10:08 +0530

    terms

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -3,6 +3,7 @@ import Style from './BaseLayout.module.scss'
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
+import Terms from "./Terms";
 import Portfolio from "./portfolio/Portfolio";
 import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
@@ -39,6 +40,8 @@ export default function BaseLayout() {
                   <Route exact path={'/'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/portfolio'} element={<Portfolio/>}/>
+                  <Route exact path={'/TermsandConditions'} element={<Terms/>}/>
+
                </Routes>
             </Grid>
             <Grid item>
