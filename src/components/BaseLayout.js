39f81b9bb1ea8983b760f431f82cf25d5f2e21e7
Dheenadhayalan perumal <69459677+dheenadhayalanperumal@users.noreply.github.com>
2025-07-11 10:12:53 +0530

    removed  copy bottom

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -61,8 +61,8 @@ export default function BaseLayout() {
           <Grid item>
             <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
               py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
-              <p>Site created with &hearts; by <a href={'https://dheena.live'}>Dheenadhayalan Perumal</a></p>
-              <p>&copy; 2024</p>
+              {/* <p>Site created with &hearts; by <a href={'https://dheena.live'}>Dheenadhayalan Perumal</a></p> */}
+              {/* <p>&copy; 2024</p> */}
             </Box>
           </Grid>
         )}
